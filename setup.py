6bebd6af5e44b9bcbaece275e8a33b561412c3c6
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2019-11-19 16:02:41 +0000

    Revert "Replace dlsio with scisoftpy import and require one of PyQt4 or PySide as dependencies"
    This reverts commit f9bf65bc3652ad55b5ccc120faf3f0ec3eb3f7f6.

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
+
 from setuptools import setup, find_packages
 
-install_deps = ['numpy>=1.6', 'scisoftpy>=2.16']
-try:
-    import PySide
-except ImportError:
-    install_deps.append('PyQt4>=4')
-
 setup(
-    name = "hrpd_rebin",
+    name = "High-resolution powder diffraction",
     version = "0.9",
     packages = find_packages(),
-    install_requires = install_deps,
+    install_requires = ['numpy>=1.6', 'pyside>=1.2'],
     entry_points = {'console_scripts': ['rebin = rebin.maincmd:main'],
                     'gui_scripts': ['rebin-gui = rebin.mainui:main']},
 )
