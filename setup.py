a019d86c80a9f450a05951528d6df926b66424a9
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2019-11-20 10:06:18 +0000

    Add classifiers, url, description and author info to setup.py

--- setup.py
+++ setup.py
@@ -7,10 +7,19 @@ except ImportError:
     install_deps.append('PyQt4>=4')
 
 setup(
-    name = "hrpd_rebin",
-    version = "1.0",
-    packages = find_packages(),
-    install_requires = install_deps,
-    entry_points = {'console_scripts': ['rebin = rebin.maincmd:main'],
+    name="hrpd_rebin",
+    version="1.0",
+    description='High-resolution powder diffraction rebin',
+    author="Peter Chang",
+    author_email="scientificsoftware@diamond.ac.uk",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+    ],
+    packages=find_packages(),
+    install_requires=install_deps,
+    entry_points={'console_scripts': ['rebin = rebin.maincmd:main'],
                     'gui_scripts': ['rebin-gui = rebin.mainui:main']},
+    url='https://github.com/DiamondLightSource/python-hrpd-rebin',
 )
