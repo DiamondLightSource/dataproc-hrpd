1205c603cc063a17e456acae4a0be0a78c4f2892
VictoriaBeilsten-Edmands <45741274+VictoriaBeilsten-Edmands@users.noreply.github.com>
2019-11-22 16:18:18 +0000

    Remove underscore from hrpd_rebin in setup.py and rename rebin to hrpdrebin (#3)
    * Remove underscore from hrpd_rebin and rename rebin dir to hrpdrebin
    
    * Rename rebin to hrpdrebin in scripts

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="hrpd_rebin",
+    name="hrpdrebin",
     version="1.0",
     description='High-resolution powder diffraction rebin',
     author="Peter Chang",
@@ -13,7 +13,7 @@ setup(
     ],
     packages=find_packages(),
     install_requires=['numpy>=1.6', 'scisoftpy>=2.16'],
-    entry_points={'console_scripts': ['rebin = rebin.maincmd:main'],
-                    'gui_scripts': ['rebin-gui = rebin.mainui:main']},
+    entry_points={'console_scripts': ['rebin = hrpdrebin.maincmd:main'],
+                    'gui_scripts': ['rebin-gui = hrpdrebin.mainui:main']},
     url='https://github.com/DiamondLightSource/python-hrpd-rebin',
 )
