fd9129198fce9636e720282452157a91bd45affa
Beilsten-Edmands, Victoria (DLSLtd,RAL,LSCI) <victoria.beilsten-edmands@diamond.ac.uk>
2019-11-28 16:19:44 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hrpdrebin",
-    version="1.0",
+    version="1.1",
     description='High-resolution powder diffraction rebin',
     author="Peter Chang",
     author_email="scientificsoftware@diamond.ac.uk",
