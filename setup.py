d7f250b99f2d98a52866a6d671ebe8d25a548fc1
VictoriaBeilsten-Edmands <45741274+VictoriaBeilsten-Edmands@users.noreply.github.com>
2019-11-29 09:12:38 +0000

    Require scisoftpy version 2.17 (#4)
    * Require scisoftpy version 2.17
    
    * Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="hrpdrebin",
-    version="1.0",
+    version="1.1",
     description='High-resolution powder diffraction rebin',
     author="Peter Chang",
     author_email="scientificsoftware@diamond.ac.uk",
@@ -12,7 +12,7 @@ setup(
         "Programming Language :: Python :: 2.7",
     ],
     packages=find_packages(),
-    install_requires=['numpy>=1.6', 'scisoftpy>=2.16'],
+    install_requires=['numpy>=1.6', 'scisoftpy>=2.17'],
     entry_points={'console_scripts': ['rebin = hrpdrebin.maincmd:main'],
                     'gui_scripts': ['rebin-gui = hrpdrebin.mainui:main']},
     url='https://github.com/DiamondLightSource/python-hrpd-rebin',
